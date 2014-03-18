56babd103c3666010207654aadd3bd2b2cb58873
eva19950630 <fun910347@gmail.com>
2014-03-18 17:34:21 +0800

    先打出gui的frame
    照著課本上frame的範例先打出框框...

---
+++ src/BMIFrame.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JFrame;
+
+public class BMIFrame {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("BMIFrame");
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
